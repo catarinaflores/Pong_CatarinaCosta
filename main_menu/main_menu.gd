3b52fde0c0fd0f80456ff1d3d9b14510bfe3d3b8
catarinaflores <catarina10flores@gmail.com>
2023-10-26 14:55:00 +0100

    singleplayer level added

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,19 +1,15 @@
 extends Control
 
-@onready var player_name_field: TextEdit = %PlayerNameField
-@onready var player_2_name_field: TextEdit = %Player2NameField
 
 
 func _on_start_game_button_pressed() -> void:
-	GameManager.player1_name = player_name_field.text
-	get_tree().change_scene_to_file("res://level/level.tscn")
+	get_tree().change_scene_to_file("res://singleplayer_menu.tscn")
 	
+
+func _on_multiplayer_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://multiplayer_menu.tscn")
+
 
 func _on_quit_game_button_pressed() -> void:
 	get_tree().quit()
 
-
-func _on_multiplayer_button_pressed() -> void:
-	GameManager.player1_name = player_name_field.text
-	GameManager.player2_name = player_2_name_field.text
-	get_tree().change_scene_to_file("res://multiplayer_menu.tscn")
---
+++ singleplayer_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+@onready var player_name_field: TextEdit = %PlayerNameField
+
+
+
+func _on_start_game_button_pressed() -> void:
+	GameManager.player1_name = player_name_field.text
+	get_tree().change_scene_to_file("res://level/level.tscn")
