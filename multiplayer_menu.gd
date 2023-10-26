3c390d8305a064c3c8c9669236a0eb89925832c1
catarinaflores <catarina10flores@gmail.com>
2023-10-26 16:36:05 +0100

    quit buttons added

--- multiplayer_menu.gd
+++ multiplayer_menu.gd
@@ -9,3 +9,7 @@ func _on_multiplayer_button_pressed() -> void:
 	GameManager.player1_name = player_name_field.text
 	GameManager.player2_name = player_2_name_field.text
 	get_tree().change_scene_to_file("res://multiplayer_level/multiplayer_level.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu/main_menu.tscn")
--- singleplayer_menu.gd
+++ singleplayer_menu.gd
@@ -8,3 +8,8 @@ extends Control
 func _on_start_game_button_pressed() -> void:
 	GameManager.player1_name = player_name_field.text
 	get_tree().change_scene_to_file("res://level/level.tscn")
+
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu/main_menu.tscn")
