64d9253f836e22a6201f7886e203ebd6cadd1461
catarinaflores <catarina10flores@gmail.com>
2023-10-26 12:48:18 +0100

    player 2

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,13 +1,17 @@
 extends Control
 
 @onready var player_name_field: TextEdit = %PlayerNameField
+@onready var player_2_name_field: TextEdit = %Player2NameField
 
 
 func _on_start_game_button_pressed() -> void:
 	GameManager.player1_name = player_name_field.text
 	get_tree().change_scene_to_file("res://level/level.tscn")
 	
-
+func _on_multiplayer_game_button_pressed() -> void:
+	GameManager.player1_name = player_name_field.text
+	GameManager.player2_name = player_2_name_field.text
+	get_tree().change_scene_to_file("res://multiplayer_level/multiplayer_level.tscn")
 
 func _on_quit_game_button_pressed() -> void:
 	get_tree().quit()
---
+++ multiplayer_level/player2_paddle.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+@export var speed := 400
+
+
+var direction := 0.0
+
+@onready var start_position := position
+
+
+func _physics_process(_delta: float) -> void:
+	position.x = 0
+	velocity.y = direction * speed
+	move_and_slide()
+	position.x = start_position.x
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	direction = Input.get_axis("ui_up", "ui_down")
